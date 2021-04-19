b3e0cf5cabef8dc4c14e89aba90e6aac71dedbc6
Caleb Stauffer <develop+github@calebstauffer.com>
2021-04-19 15:39:01 -0400

    add environment adjustments

--- cssllc-environment.php
+++ cssllc-environment.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Description: Force explicit setting of environment type.
+ * Description: Environment identification and adjustments.
  * Author URI: https://develop.calebstauffer.com
  * Author: Caleb Stauffer
  * Plugin URI: https://github.com/cssllc/mu-plugins
@@ -49,6 +49,148 @@ if ( !function_exists( 'cssllc_require_set_environment_type' ) ) {
 		exit;
 	}
 
-	cssllc_require_set_environment_type();
+}
+
+cssllc_require_set_environment_type();
+
+
+/*
+   ###    ########        ## ##     ##  ######  ######## ##     ## ######## ##    ## ########  ######
+  ## ##   ##     ##       ## ##     ## ##    ##    ##    ###   ### ##       ###   ##    ##    ##    ##
+ ##   ##  ##     ##       ## ##     ## ##          ##    #### #### ##       ####  ##    ##    ##
+##     ## ##     ##       ## ##     ##  ######     ##    ## ### ## ######   ## ## ##    ##     ######
+######### ##     ## ##    ## ##     ##       ##    ##    ##     ## ##       ##  ####    ##          ##
+##     ## ##     ## ##    ## ##     ## ##    ##    ##    ##     ## ##       ##   ###    ##    ##    ##
+##     ## ########   ######   #######   ######     ##    ##     ## ######## ##    ##    ##     ######
+*/
+
+$dev_email_address = 'develop@calebstauffer.com';
+
+if ( empty( $dev_email_address ) )
+	trigger_error( 'Update <code>$dev_email_address</code> in ' . __FILE__, E_USER_WARNING );
+
+/**
+ * Check if production environment.
+ */
+if ( 'production' === wp_get_environment_type() ) {
+
+	/**
+	 * Add inactive status indication to mu-plugin row meta.
+	 */
+	add_filter( 'plugin_row_meta', function ( array $meta, string $plugin_file ) : array {
+		if ( basename( __FILE__ ) !== $plugin_file )
+			return $meta;
+
+		array_unshift( $meta, '<strong>Inactive</strong>' );
+		return $meta;
+	}, 10, 2 );
+
+	return;
 
 }
+
+/**
+ * Action: admin_notices
+ *
+ * Add a notice in admin so we're aware of the preventions.
+ *
+ * @return void
+ */
+add_action( 'admin_notices', function() : void {
+	echo '<div class="notice notice-warning">' .
+		'<p>Functionality adjustments due to development environment; see <abbr title="' . esc_attr( __FILE__ ) . '">' . basename( __FILE__ ) . '</abbr> mu-plugin.</p>' .
+	'</div>';
+}, -999 );
+
+/**
+ * Filter: plugin_row_meta
+ *
+ * Add active status indication to mu-plugin row meta.
+ *
+ * @param array $meta
+ * @param string $plugin_file
+ * @return array
+ */
+add_filter( 'plugin_row_meta', function ( array $meta, string $plugin_file ) : array {
+	if ( basename( __FILE__ ) !== $plugin_file )
+		return $meta;
+
+	array_unshift( $meta, '<strong style="color: red;">!!! ACTIVE !!!</strong>' );
+	return $meta;
+}, 10, 2 );
+
+/**
+ * Set staging constant for Square payment gateway.
+ */
+define( 'WC_SQUARE_ENABLE_STAGING', true );
+
+/**
+ * Filters: pre_option_admin_email, pre_site_option_admin_email
+ *
+ * Override WordPress admin email.
+ *
+ * @param string $email_address
+ * @return string
+ */
+add_filter(      'pre_option_admin_email', function ( string $email_address ) use ( $dev_email_address ) : string { return $dev_email_address; } );
+add_filter( 'pre_site_option_admin_email', function ( string $email_address ) use ( $dev_email_address ) : string { return $dev_email_address; } );
+
+/**
+ * Filter: wp_mail
+ *
+ * Direct all emails to development (prevent emails to customers).
+ *
+ * @param array $args
+ * @return array
+ */
+add_filter( 'wp_mail', function ( array $args ) use ( $dev_email_address ) : array { $args['to'] = $dev_email_address; return $args; }, 999 );
+
+/**
+ * Prevent running of AutomateWoo workflows.
+ *
+ * @link https://automatewoo.com/ Plugin site.
+ * @see AutomateWoo\Workflow::run()
+ * @since AutomateWoo 2.6.6
+ */
+define( 'AW_PREVENT_WORKFLOWS', true );
+
+/**
+ * Filter: woocommerce_subscriptions_is_duplicate_site
+ *
+ * Prevent WooCommerce Subscription renewals.
+ *
+ * @link https://woocommerce.com/products/woocommerce-subscriptions/ Plugin site.
+ * @see WC_Subscriptions::is_duplicate_site()
+ *
+ * @param bool
+ * @uses __return_true()
+ * @return true
+ */
+add_filter( 'woocommerce_subscriptions_is_duplicate_site', '__return_true' );
+
+/**
+ * Filter: automatewoo_custom_validate_workflow
+ *
+ * Prevent processing of AutomateWoo workflows.
+ *
+ * @link https://automatewoo.com/ Plugin site.
+ * @see AutomateWoo\Workflow::validate_workflow()
+ *
+ * @param bool
+ * @uses __return_false()
+ * @return false
+ */
+add_filter( 'automatewoo_custom_validate_workflow', '__return_false' );
+
+/**
+ * Filter: user_has_cap
+ *
+ * Enable Query Monitor output.
+ *
+ * @param array $allcaps
+ * @return array
+ */
+add_filter( 'user_has_cap', function( array $allcaps ) : array {
+	$allcaps['view_query_monitor'] = true;
+	return $allcaps;
+} );
