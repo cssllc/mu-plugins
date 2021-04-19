a5a25109ce4756bdaf73f8bc4246713ff11b6cd5
Caleb Stauffer <crstauf@users.noreply.github.com>
2021-04-19 16:35:01 -0400

    improve cpt registrar abstract
    closes #4

--- cssllc-cpt.php
+++ cssllc-cpt.php
@@ -15,30 +15,56 @@ defined( 'ABSPATH' ) || die();
  */
 abstract class _CSSLLC_CPT {
 
-	/** @var string Post type name. */
-	protected $type;
-
-	/** @var array Name and action for nonce. */
-	protected $nonce;
-
-	/** @var string Plural name of post type. */
-	protected $plural;
-
-	/** @var string Singular name of post type. */
-	protected $singular;
+	/**
+	 * @var string Custom post type slug.
+	 */
+	const TYPE = '';
 
 	/**
-	 * @var string CSS 'content' code of Dashicon to represent post type.
-	 * @link https://developer.wordpress.org/resource/dashicons/
+	 *@var string Singular label.
 	 */
-	protected $dashicon_code;
+	const SINGULAR = '';
 
-	/** @var array Arguments for post type. */
-	protected $args = array(
-		'labels'   => array(),
-		'rewrite'  => array(),
-		'supports' => array(),
+	/**
+	 * @var string Plural label.
+	 */
+	const PLURAL = '';
+
+	/**
+	 * @var string CSS font code for Dashicon (ex: '\\f120').
+	 * @link https://developer.wordpress.org/resource/dashicons/#wordpress Collection.
+	 */
+	const DASHICON_CODE = '';
+
+	/**
+	 * @var array Arguments for custom post type.
+	 */
+	protected $args = array();
+
+	/**
+	 * @var array Default arguments for custom post type.
+	 */
+	private static $default_args = array(
+		'public'       => true,
+		'has_archive'  => true,
+		'hierarchical' => false,
+
+		'rewrite' => array(
+			'with_front' => false,
+		),
+
 	);
+
+
+	/*
+	 ######  ########    ###    ######## ####  ######
+	##    ##    ##      ## ##      ##     ##  ##    ##
+	##          ##     ##   ##     ##     ##  ##
+	 ######     ##    ##     ##    ##     ##  ##
+	      ##    ##    #########    ##     ##  ##
+	##    ##    ##    ##     ##    ##     ##  ##    ##
+	 ######     ##    ##     ##    ##    ####  ######
+	*/
 
 	/**
 	 * Initialize.
@@ -59,9 +85,9 @@ abstract class _CSSLLC_CPT {
 	/**
 	 * Create or get instance.
 	 *
-	 * @return self
+	 * @return _CSSLLC_CPT
 	 */
-	static function instance() : self {
+	static function instance() : _CSSLLC_CPT {
 		static $instance = null;
 
 		if ( is_null( $instance ) )
@@ -71,29 +97,121 @@ abstract class _CSSLLC_CPT {
 	}
 
 	/**
+	 * Register the post type.
+	 *
+	 * @uses static::instance()
+	 * @uses register_post_type()
+	 * @link https://developer.wordpress.org/reference/functions/register_post_type/#parameter-detail-information Parameters documentation.
+	 * @return void
+	 */
+	protected static function register() : void {
+		if (
+			   empty( static::TYPE     )
+			|| empty( static::SINGULAR )
+			|| empty( static::PLURAL   )
+		) {
+			trigger_error( sprintf( 'Custom post type `<code>%s</code>` is not setup.', static::TYPE ), E_USER_WARNING );
+			return;
+		}
+
+		$args = wp_parse_args( static::instance()->args, static::$default_args );
+		register_post_type( static::TYPE, $args );
+	}
+
+	/**
+	 * Generate labels for post type.
+	 *
+	 * @link https://developer.wordpress.org/reference/functions/get_post_type_labels/ List of labels.
+	 * @return array
+	 */
+	protected static function default_labels() : array {
+		return array(
+			'name'                  => static::PLURAL,
+			'singular_name'         => static::SINGULAR,
+			'add_new'               => 'Add ' . static::SINGULAR,
+			'add_new_item'          => 'Add New ' . static::SINGULAR,
+			'edit_item'             => 'Edit ' . static::SINGULAR,
+			'new_item'              => 'New ' . static::SINGULAR,
+			'view_item'             => 'View ' . static::SINGULAR,
+			'search_items'          => 'Search ' . static::PLURAL,
+			'not_found'             => 'No ' . strtolower( static::PLURAL ) . ' found',
+			'not_found_in_trash'    => 'No ' . strtolower( static::PLURAL ) . ' found in Trash',
+			'parent_item_colon'     => 'Parent ' . static::PLURAL . ':',
+			'all_items'             => 'All ' . static::PLURAL,
+			'archives'              => static::SINGULAR . ' Archives',
+			'attributes'            => static::SINGULAR . ' Attributes',
+			'insert_into_item'      => 'Insert into ' . strtolower( static::SINGULAR ),
+			'uploaded_to_this_item' => 'Uploaded to this ' . strtolower( static::SINGULAR ),
+			// 'featured_image'        => 'Featured Image',
+			// 'set_featured_image'    => 'Set Featured Image',
+			// 'remove_featured_image' => 'Remove featured image',
+			// 'use_featured_image'    => 'Use as featured image',
+			'menu_name'             => static::PLURAL,
+			// 'filter_items_list'     => static::PLURAL,
+			// 'items_list_navigation' => static::PLURAL,
+			'items_list'            => static::PLURAL,
+			'name_admin_bar'        => static::SINGULAR,
+		);
+	}
+
+	/**
+	 * Get WordPress post type object.
+	 *
+	 * @uses get_post_type_object()
+	 * @return WP_Post_Type
+	 */
+	static function object() : WP_Post_Type {
+		$object = get_post_type_object( static::TYPE );
+
+		if ( is_null( $object ) )
+			return new WP_Post_Type( 'does_not_exist' );
+
+		return $object;
+	}
+
+
+	/*
+	#### ##    ##  ######  ########    ###    ##    ##  ######  ########
+	 ##  ###   ## ##    ##    ##      ## ##   ###   ## ##    ## ##
+	 ##  ####  ## ##          ##     ##   ##  ####  ## ##       ##
+	 ##  ## ## ##  ######     ##    ##     ## ## ## ## ##       ######
+	 ##  ##  ####       ##    ##    ######### ##  #### ##       ##
+	 ##  ##   ### ##    ##    ##    ##     ## ##   ### ##    ## ##
+	#### ##    ##  ######     ##    ##     ## ##    ##  ######  ########
+	*/
+
+	/**
 	 * Construct.
+	 *
+	 * @uses $this->setup()
 	 */
 	protected function __construct() {
-		$this->nonce = array(
-			'action' => __FILE__ . '::' . __LINE__,
-			  'name' => '_wpnonce_' . $this->type,
-		);
+		$this->setup();
 
 		add_action( 'init',       array( $this, 'action__init'       ) );
 		add_action( 'admin_init', array( $this, 'action__admin_init' ) );
 
-		add_filter( 'dashboard_glance_items', array( $this, 'filter__dashboard_glance_items' ) );
-		add_filter( 'post_updated_messages',  array( $this, 'filter__post_updated_messages'  ) );
+		add_filter( 'dashboard_glance_items',     array( $this, 'filter__dashboard_glance_items' ) );
+		add_filter( 'bulk_post_updated_messages', array( $this, 'filter__bulk_post_updated_messages' ), 10, 2 );
+		add_filter( 'post_updated_messages',      array( $this, 'filter__post_updated_messages'  ) );
+
 	}
 
 	/**
-	 * Getter.
+	 * Setup properties.
 	 *
-	 * @param string $property
-	 * @return mixed
+	 * @uses static::default_labels()
+	 * @return void
 	 */
-	function __get( string $property ) {
-		return $this->$property;
+	protected function setup() : void {
+		if ( !array_key_exists( 'labels',  $this->args ) ) $this->args['labels']  = array();
+		if ( !array_key_exists( 'rewrite', $this->args ) ) $this->args['rewrite'] = array();
+
+		if ( is_array( $this->args['labels'] ) )
+			$this->args['labels'] = wp_parse_args( $this->args['labels'], static::default_labels() );
+
+		if ( is_array( $this->args['rewrite'] ) )
+			$this->args['rewrite'] = wp_parse_args( $this->args['rewrite'], static::$default_args['rewrite'] );
 	}
 
 
@@ -110,111 +228,31 @@ abstract class _CSSLLC_CPT {
 	/**
 	 * Action: init
 	 *
-	 * @uses register_post_type()
-	 * @link https://developer.wordpress.org/reference/functions/register_post_type/#parameter-detail-information Parameters documentation.
+	 * Register the post type.
+	 *
+	 * @uses static::register()
 	 * @return void
 	 */
 	function action__init() : void {
+		if ( 'init' !== current_action() )
+			return;
 
-		$label_defaults = array(
-			'name'                  => $this->plural,
-			'singular_name'         => $this->singular,
-			'add_new'               => 'Add ' . $this->singular,
-			'add_new_item'          => 'Add New ' . $this->singular,
-			'edit_item'             => 'Edit ' . $this->singular,
-			'new_item'              => 'New ' . $this->singular,
-			'view_item'             => 'View ' . $this->singular,
-			'search_items'          => 'Search ' . $this->plural,
-			'not_found'             => 'No ' . strtolower( $this->plural ) . ' found',
-			'not_found_in_trash'    => 'No ' . strtolower( $this->plural ) . ' found in Trash',
-			'parent_item_colon'     => 'Parent ' . $this->plural . ':',
-			'all_items'             => 'All ' . $this->plural,
-			'archives'              => $this->singular . ' Archives',
-			'attributes'            => $this->singular . ' Attributes',
-			'insert_into_item'      => 'Insert into ' . strtolower( $this->singular ),
-			'uploaded_to_this_item' => 'Uploaded to this ' . strtolower( $this->singular ),
-			// 'featured_image'        => 'Featured Image',
-			// 'set_featured_image'    => 'Set Featured Image',
-			// 'remove_featured_image' => 'Remove featured image',
-			// 'use_featured_image'    => 'Use as featured image',
-			'menu_name'             => $this->plural,
-			// 'filter_items_list'     => $this->plural,
-			// 'items_list_navigation' => $this->plural,
-			'items_list'            => $this->plural,
-			'name_admin_bar'        => $this->singular,
-		);
-
-		if ( function_exists( 'create_post_type_labels' ) )
-			$label_defaults = create_post_type_labels( $this->singular, $this->plural );
-
-		$labels = wp_parse_args(
-			(
-				!empty( $this->args['labels'] )
-				? $this->args['labels']
-				: array()
-			),
-			$label_defaults
-		);
-
-		foreach ( array( 'labels', 'rewrite', 'supports' ) as $arg )
-			if (
-				array_key_exists( $arg, $this->args )
-				&& array() === $this->args[ $arg ]
-			)
-				 unset( $this->args[ $arg ] );
-
-		$args = wp_parse_args( $this->args, array(
-			'labels'             => $labels,
-			'public'             => true,
-			'has_archive'        => true,
-			'hierarchical'       => false,
-			'show_in_rest'       => true,
-		) );
-
-		if (
-			     !empty( $this->args['rewrite'] )
-			&& is_array( $this->args['rewrite'] )
-		)
-			$args['rewrite'] = wp_parse_args(
-				(
-					!empty( $this->args['rewrite'] )
-					? $this->args['rewrite']
-					: array()
-				),
-				array(
-					'slug'       => $this->type,
-					'with_front' => false,
-				)
-			);
-
-		$args['supports'] = array(
-			'title',
-			'author',
-			'editor',
-			'excerpt',
-			'thumbnail',
-		);
-
-		if (
-			       !empty( $this->args['supports'] )
-			&& array() !== $this->args['supports']
-		)
-			$args['supports'] = $this->args['supports'];
-
-		register_post_type( $this->type, $args );
-
+		static::register();
 	}
 
 	/**
 	 * Action: admin_init
 	 *
+	 * Add CSS class for dashicon.
+	 *
+	 * @uses wp_add_inline_style()
 	 * @return void
 	 */
 	function action__admin_init() : void {
-		if ( empty( $this->dashicon_code ) )
+		if ( 'admin_init' !== current_action() )
 			return;
 
-		wp_add_inline_style( 'dashicons', '.icon-cpt-' . $this->type . ':before { content: "' . $this->dashicon_code . '" !important; }' );
+		wp_add_inline_style( 'dashicons', '.icon-cpt-' . esc_attr( static::TYPE ) . ':before { content: "' . static::DASHICON_CODE . '" !important; }' );
 	}
 
 
@@ -229,98 +267,137 @@ abstract class _CSSLLC_CPT {
 	*/
 
 	/**
+	 * Filter: dashboard_glance_items
+	 *
 	 * Add count of CPT to 'At a Glance' dashboard widget.
 	 *
-	 * @param array $items
-	 * @return array
+	 * @uses static::object()
+	 * @uses wp_count_posts()
+	 * @param string[] $items
+	 * @return string[]
 	 */
 	function filter__dashboard_glance_items( array $items ) : array {
-		$object = get_post_type_object( $this->type );
-
-		if ( !current_user_can( $object->cap->edit_posts ) )
+		if ( 'dashboard_glance_items' !== current_filter() )
 			return $items;
 
-		$count = wp_count_posts( $this->type );
+		if ( !current_user_can( static::object()->cap->edit_posts ) )
+			return $items;
+
+		$count = wp_count_posts( static::TYPE );
 
 		if ( empty( $count->publish ) )
 			$count->publish = 0;
 
-		$items['count_' . $this->type] =
-			'<a class="icon-cpt-' . $this->type . '" href="' . admin_url( add_query_arg( 'post_type', $this->type, 'edit.php' ) ) . '">' .
-				$count->publish . _n( ' ' . $this->singular, ' ' . $this->plural, $count->publish ) .
-			'</a>';
+		$url = add_query_arg( 'post_type', static::TYPE, 'edit.php' );
+
+		$item  = '<a class="icon-cpt-' . esc_attr( static::TYPE ) . '" href="' . esc_attr( esc_url( $url ) ) . '">';
+		$item .= $count->publish . ' ' . _n( static::SINGULAR, static::PLURAL, $count->publish );
+		$item .= '</a>';
+
+		$items[] = $item;
 
 		return $items;
 	}
 
 	/**
-	 * Add messages for updating the custom post type.
+	 * Filter: bulk_post_updated_messages
+	 *
+	 * Add messages for bulk updating CPT posts.
+	 *
+	 * @param array $bulk_messages
+	 * @param array $bulk_counts
+	 * @return array
+	 */
+	function filter__bulk_post_updated_messages( array $bulk_messages, array $bulk_counts ) : array {
+		if ( 'bulk_post_updated_messages' !== current_filter() )
+			return $bulk_messages;
+
+		$singular = strtolower( static::SINGULAR );
+		$plural   = strtolower( static::PLURAL   );
+
+		$bulk_messages[static::TYPE] = array(
+			'updated'   => _n( '%s ' . $singular . ' updated.',                 '%s ' . $plural . ' updated.',                 $bulk_counts['updated']   ),
+			'deleted'   => _n( '%s ' . $singular . ' permanently deleted.',     '%s ' . $plural . ' permanently deleted.',     $bulk_counts['deleted']   ),
+			'trashed'   => _n( '%s ' . $singular . ' moved to the Trash.',      '%s ' . $plural . ' moved to the Trash.',      $bulk_counts['trashed']   ),
+			'untrashed' => _n( '%s ' . $singular . ' restored from the Trash.', '%s ' . $plural . ' restored from the Trash.', $bulk_counts['untrashed'] ),
+		);
+
+		$bulk_messages[static::TYPE]['locked'] = _n(
+			'%s ' . $singular . ' not updated, somebody is editing it.',
+			'%s ' . $plural   . ' not updated, somebody is editing them.',
+			$bulk_counts['locked']
+		);
+
+		return $bulk_messages;
+	}
+
+	/**
+	 * Filter: post_updated_messages
+	 *
+	 * Add messages for updating CPT post.
 	 *
 	 * @param array $notices
 	 * @return array
 	 */
 	function filter__post_updated_messages( array $notices ) : array {
-		global $post_ID, $post;
-
-		if ( get_post_type( $post ) !== $this->type )
+		if ( 'post_updated_messages' !== current_filter() )
 			return $notices;
 
-		$object = get_post_type_object( get_post_type( $post ) );
+		global $post;
 
-		$notices[$this->type] = array(
+		if ( get_post_type( $post ) !== static::TYPE )
+			return $notices;
+
+		$preview_link_html   = '';
+		$scheduled_link_html = '';
+		$view_link_html      = '';
+
+		$permalink   = get_permalink( $post );
+		$preview_url = get_preview_post_link( $post );
+
+		if ( is_post_type_viewable( static::object() ) ) {
+
+			$preview_link_html = sprintf(
+				' <a href="%1$s">%2$s</a>',
+				esc_url( $preview_url ),
+				__( 'Preview ' . strtolower( static::SINGULAR ) )
+			);
+
+			$scheduled_link_html = sprintf(
+				' <a href="%1$s">%2$s</a>',
+				esc_url( $permalink ),
+				__( 'Preview ' . strtolower( static::SINGULAR ) )
+			);
+
+			$view_link_html = sprintf(
+				' <a href="%1$s">%2$s</a>',
+				esc_url( $permalink ),
+				__( 'View ' . strtolower( static::SINGULAR ) )
+			);
+
+		}
+
+		$scheduled_date = sprintf(
+			__( '%1$s at %2$s' ),
+			date_i18n( _x( 'M j, Y', 'publish box date format' ), strtotime( $post->post_date ) ),
+			date_i18n( _x( 'H:i',    'publish box time format' ), strtotime( $post->post_date ) )
+		);
+
+		$notices[static::TYPE] = array(
 			 0 => '', // Unused. Messages start at index 1.
-			 1 => sprintf( __( $this->singular . ' updated.' . ( true === $object->public ? ' <a href="%s">View ' . $this->singular . '</a>' : '' ) ), esc_url( get_permalink( $post_ID ) ) ),
+			 1 => __( static::SINGULAR . ' updated.' ) . $view_link_html,
 			 2 => __( 'Custom field updated.' ),
 			 3 => __( 'Custom field deleted.' ),
-			 4 => __( $this->singular . ' updated.' ),
-			 5 => isset( $_GET['revision'] ) ? sprintf( __( $this->singular . ' restored to revision from %s' ), wp_post_revision_title( ( int ) $_GET['revision'], false ) ) : false,
-			 6 => sprintf( __( $this->singular . ' published.' . ( true === $object->public ? ' <a href="%s">View ' . $this->singular . '</a>' : '' ) ), esc_url( get_permalink( $post_ID ) ) ),
-			 7 => __( $this->singular . ' saved.'),
-			 8 => sprintf( __( $this->singular . ' submitted. <a target="_blank" href="%s">Preview ' . $this->singular . '</a>' ), esc_url( add_query_arg( 'preview', 'true', get_permalink( $post_ID ) ) ) ),
-			 9 => sprintf( __( $this->singular . ' scheduled for: <strong>%1$s</strong>. <a target="_blank" href="%2$s">Preview ' . $this->singular . '</a>' ), date_i18n( __( 'M j, Y @ G:i' ), strtotime( $post->post_date ) ), esc_url( get_permalink( $post_ID ) ) ),
-			10 => sprintf( __( $this->singular . ' draft updated.' . ( true === $object->public ? ' <a target="_blank" href="%s">Preview ' . $this->singular . '</a>' : '' ) ), esc_url( add_query_arg( 'preview', 'true', get_permalink( $post_ID ) ) ) ),
+			 4 => __( static::SINGULAR . ' updated.' ),
+			 5 => isset( $_GET['revision'] ) ? sprintf( __( static::SINGULAR . ' restored to revision from %s.' ), wp_post_revision_title( (int) $_GET['revision'], false ) ) : false,
+			 6 => __( static::SINGULAR . ' published.' ) . $view_link_html,
+			 7 => __( static::SINGULAR . ' saved.' ),
+			 8 => __( static::SINGULAR . ' submitted.' ) . $preview_link_html,
+			 9 => sprintf( __( static::SINGULAR . ' scheduled for: %s.' ), '<strong>' . $scheduled_date . '</strong>' ) . $scheduled_link_html,
+			10 => __( static::SINGULAR . ' draft updated.' ) . $preview_link_html,
 		);
 
 		return $notices;
-	}
-
-
-	/*
-	######## ##     ## ##    ##  ######  ######## ####  #######  ##    ##  ######
-	##       ##     ## ###   ## ##    ##    ##     ##  ##     ## ###   ## ##    ##
-	##       ##     ## ####  ## ##          ##     ##  ##     ## ####  ## ##
-	######   ##     ## ## ## ## ##          ##     ##  ##     ## ## ## ##  ######
-	##       ##     ## ##  #### ##          ##     ##  ##     ## ##  ####       ##
-	##       ##     ## ##   ### ##    ##    ##     ##  ##     ## ##   ### ##    ##
-	##        #######  ##    ##  ######     ##    ####  #######  ##    ##  ######
-	*/
-
-	/**
-	 * Get nonce for CPT.
-	 *
-	 * @uses $this->get_nonce__action()
-	 * @return string
-	 */
-	function get_nonce() : string {
-		return wp_create_nonce( $this->get_nonce_action() );
-	}
-
-	/**
-	 * Get nonce action for CPT.
-	 *
-	 * @return string
-	 */
-	function get_nonce_action() : string {
-		return $this->nonce['action'];
-	}
-
-	/**
-	 * Get nonce name for CPT.
-	 *
-	 * @return string
-	 */
-	function get_nonce_name() : string {
-		return $this->nonce['name'];
 	}
 
 }
