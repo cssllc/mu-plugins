1df59c64f98b6dfc8d72a8b16735ca921682cd59
Caleb Stauffer <crstauf@users.noreply.github.com>
2022-05-28 10:51:45 -0400

    cleanup unnecessary core code

--- cssllc-enhancements.php
+++ cssllc-enhancements.php
@@ -16,6 +16,8 @@ add_action( 'wp_enqueue_scripts', function() {
 	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
 	remove_action( 'wp_print_styles',     'print_emoji_styles' );
 	remove_action( 'admin_print_styles',  'print_emoji_styles' );
+	remove_action( 'wp_enqueue_scripts',  'wp_enqueue_global_styles' );
+	remove_action( 'wp_body_open',        'wp_global_styles_render_svg_filters' );
 } );
 
 /**
